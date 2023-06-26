4bcd97487662ecc778dafaa9259d7ef941abe751
Yale Araujo <yale850@gmail.com>
2023-06-26 17:24:06 -0300

    styles: button on top

--- src/app/dashboard/clients/page.tsx
+++ src/app/dashboard/clients/page.tsx
@@ -7,16 +7,19 @@ import { ClientsList } from './components/ClientsList'
 export default async function ClientList() {
   return (
     <div>
-      <Heading size="lg">Meus clientes</Heading>
-      <main className="w-full mx-auto text-gray-100">
-        <ClientsList />
+      <div className="flex justify-between items-center">
+        <Heading size="lg">Meus clientes</Heading>
         <div className="w-56 py-5 ml-auto">
           <Button asChild>
             <Link href="/dashboard/clients/new" className="block text-center">
-              Cadastrar Novo Cliente
+              Adicionar novo investidor
             </Link>
           </Button>
         </div>
+      </div>
+
+      <main className="w-full mx-auto text-gray-100">
+        <ClientsList />
       </main>
     </div>
   )
