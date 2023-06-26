8840da5e55d0c34cd34b92b19ff8d78a14932549
Yale Araujo <yale850@gmail.com>
2023-06-25 22:47:23 -0300

    feat: select input component

---
+++ src/components/Form/SelectInput/index.tsx
@@ -0,0 +1,57 @@
+import * as Select from '@radix-ui/react-select'
+import { CaretDown, Check } from '@/assets/icons/phosphorIcons'
+import { clsx } from 'clsx'
+import { ForwardRefRenderFunction, ReactNode, forwardRef } from 'react'
+
+interface SelectInputProps extends Select.SelectTriggerProps {
+  children: ReactNode
+}
+
+export function SelectInput({ children, ...props }: SelectInputProps) {
+  return (
+    <Select.Root>
+      <Select.Trigger
+        {...props}
+        className={clsx(
+          'bg-gray-800 text-gray-100 text-sm py-4 px-3 w-full flex items-center justify-between hover:bg-gray-70 rounded-md',
+        )}
+      >
+        <Select.Value placeholder="John Do da Silva" />
+        <Select.Icon>
+          <CaretDown />
+        </Select.Icon>
+      </Select.Trigger>
+
+      <Select.Portal>
+        <Select.Content className="overflow-hidden bg-gray-300 rounded-md">
+          <Select.Viewport className="p-2">{children}</Select.Viewport>
+          <Select.Arrow />
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  )
+}
+
+interface SelectItemProps extends Select.SelectItemProps {
+  children: ReactNode
+}
+
+const SelectItemBase: ForwardRefRenderFunction<
+  HTMLDivElement,
+  SelectItemProps
+> = ({ children, ...props }, ref) => {
+  return (
+    <Select.Item
+      ref={ref}
+      className="text-sm text-gray-900 rounded-sm flex p-2 relative select-none items-center gap-2 focus:border-none focus:outline-none"
+      {...props}
+    >
+      <Select.ItemText>{children}</Select.ItemText>
+      <Select.ItemIndicator>
+        <Check />
+      </Select.ItemIndicator>
+    </Select.Item>
+  )
+}
+
+export const SelectItem = forwardRef(SelectItemBase)
