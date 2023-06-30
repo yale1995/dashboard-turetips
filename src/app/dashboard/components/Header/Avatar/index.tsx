417bbaf0096f681ed2514f38190c0099f1262584
Yale Araujo <yale850@gmail.com>
2023-06-30 12:00:10 -0300

    feat: user avatar consuming initial letters

--- src/app/dashboard/components/Header/Avatar/index.tsx
+++ src/app/dashboard/components/Header/Avatar/index.tsx
@@ -1,16 +1,21 @@
 'use client'
 import * as RadixAvatar from '@radix-ui/react-avatar'
 
-export function Avatar() {
+interface AvatarProps {
+  userInitials: string
+  sourceImg: string | undefined
+}
+
+export function Avatar({ sourceImg, userInitials }: AvatarProps) {
   return (
     <RadixAvatar.Root className="inline-flex items-center justify-center overflow-hidden w-16 h-16 rounded-full bg-gray-800">
       <RadixAvatar.Image
         className="w-full h-full object-cover rounded-full"
-        src="http://github.com/caiorodrigo1.png"
+        src={sourceImg}
         alt="profile image"
       />
       <RadixAvatar.Fallback className="text-2xl flex justify-center items-center w-full h-full bg-gray-300 text-indigo-600">
-        CR
+        {userInitials}
       </RadixAvatar.Fallback>
     </RadixAvatar.Root>
   )
